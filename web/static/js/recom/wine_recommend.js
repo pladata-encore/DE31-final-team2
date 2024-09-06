3def88cd43070e6559a53044cc13985f78a66aa0
hyun <mhbsh@naver.com>
2024-09-06 15:10:23 +0900

    0906 답변 섹션 디자인 및 구성 변경

--- web/static/js/recom/wine_recommend.js
+++ web/static/js/recom/wine_recommend.js
@@ -3,7 +3,7 @@ const qna = document.querySelector(".qna");
 const food = document.querySelector(".food_info");
 const flavor = document.querySelector(".flavor_info");
 const taste = document.querySelector(".taste_info");
-const main = document.querySelector(".main_info");
+const profile = document.querySelector(".profile_info");
 const result = document.querySelector(".result");
 
 // start -> food
@@ -17,19 +17,38 @@ function begin(){
 }
 
 // food -> flavor
+// function next1(){
+//     food.style.display = "none";
+//     flavor.style.display = "block";
+
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
+
+// food -> flavor : 답변 필수 선택하게 하는 코드
 function next1(){
-    food.style.display = "none";
-    flavor.style.display = "block";
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    const selectFood = document.querySelector('input[name="food"]:checked');
+
+    if (!selectFood) {
+        alert("답변을 선택해 주세요.");
+    } else {
+        food.style.display = "none";
+        flavor.style.display = "block";
+        
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
 }
 
 // food <- flavor
 function pre1(){
     flavor.style.display = "none";
     food.style.display = "block";
+
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
@@ -37,49 +56,92 @@ function pre1(){
 }
 
 // flavor -> taste
+// function next2(){
+//     flavor.style.display = "none";
+//     taste.style.display = "block";
+
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
+
+// flavor -> taste : 답변 필수 선택하게 하는 코드
 function next2(){
-    flavor.style.display = "none";
-    taste.style.display = "block";
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    const selectFlavor = document.querySelector('input[name="flavor"]:checked');
+
+    if (!selectFlavor) {
+        alert("답변을 선택해 주세요.")
+    } else {
+        flavor.style.display = "none";
+        taste.style.display = "block";
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
 }
 
 // flavor <- taste
 function pre2(){
     taste.style.display = "none";
     flavor.style.display = "block";
+
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
 
-// taste -> main
+// taste -> profile
+// function next3(){
+//     taste.style.display = "none";
+//     profile.style.display = "block";
+
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
+
+// taste -> profile : 답변 필수 선택하게 하는 코드
 function next3(){
-    taste.style.display = "none";
-    main.style.display = "block";
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    const selectSweetness = document.querySelector('input[name="sweetness"]:checked');
+    const selectAcidity = document.querySelector('input[name="acidity"]:checked');
+    const selectTannin = document.querySelector('input[name="tannin"]:checked');
+    const selectIntensity = document.querySelector('input[name="intensity"]:checked');
+    const selectBody = document.querySelector('input[name="body"]:checked');
+
+    if (!selectSweetness || !selectAcidity || !selectTannin || !selectIntensity || !selectBody) {
+        alert("답변을 선택해 주세요")
+    } else {
+        taste.style.display = "none";
+        profile.style.display = "block";
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
 }
 
-// taste <- main
+// taste <- profile
 function pre3(){
-    main.style.display = "none";
+    profile.style.display = "none";
     taste.style.display = "block";
+
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
 
-// main -> result
+// profile -> result
 function end(){
-    main.style.display = "none";
+    profile.style.display = "none";
     result.style.display = "block";
+
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
