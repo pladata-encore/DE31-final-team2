af7cad24d24043ba504f6e34ecdf28a54080dd5c
hyun <mhbsh@naver.com>
2024-09-24 15:17:18 +0900

    파일 정리

--- web/static/js/fixed.js
+++
@@ -1,9 +0,0 @@
-// 사이드 메뉴 열기 버튼 클릭 시
-document.querySelector('.sideMenu').addEventListener('click', function() {
-    document.querySelector('.detail_user_menu').classList.toggle('active');
-});
-
-// 사이드 메뉴 닫기 버튼 클릭 시
-document.querySelector('.sideClose').addEventListener('click', function() {
-    document.querySelector('.detail_user_menu').classList.remove('active');
-});
